603f2dbb47bbcb825f4bd9390140604f32718dc7
leomissing <leomissing@yandex.ru>
2023-09-11 18:23:30 +0300

    Minor fixes

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 class Game:
 
-    def __init__(self):
+    def __init__(self, players):
         self.board = [["_"] * 6 for _ in range(7)]
         self.turn = 0
         self.win = False
+        self.players = int(players)
+        self.player = 0
 
     def get_board(self):
         print("| 1 | 2 | 3 | 4 | 5 | 6 | 7 |")
-        for x in range(5, -1, -1):
-            print("|", self.board[0][x], "|", self.board[1][x], "|", self.board[2][x], "|", self.board[3][x], "|",
-                  self.board[4][x], "|", self.board[5][x], "|", self.board[6][x], "|")
+        for row in range(5, -1, -1):
+            row_str = "|"
+            for col in range(7):
+                row_str += f" {self.board[col][row]} |"
+            print(row_str)
 
     def add_token(self, column):
         i = self.board[column].index("_")
-        self.board[column][i] = self.turn % 2
+        self.board[column][i] = self.turn % self.players
 
     def check_win_cond(self):
         def check_line(line):
@@ -80,7 +84,7 @@ class Game:
 
     def start(self):
         while not self.win:
-            print("Player № ", self.turn % 2 + 1, " move")
+            print("Player № ", self.player, " move")
             self.get_board()
 
             try:
@@ -93,11 +97,14 @@ class Game:
             self.add_token(column=(column - 1))
             self.check_win_cond()
             self.turn += 1
-        print("The winner is Player №", self.turn % 2 )
+            self.player = self.turn % self.players
+        self.turn -=1
+        self.player = self.turn % self.players
+        print("The winner is Player №", self.player )
         self.get_board()
 
 
 # Create an instance of the game and start it
 if __name__ == "__main__":
-    connect4 = Game()
+    connect4 = Game(players=input("Input Number of players"))
     connect4.start()
